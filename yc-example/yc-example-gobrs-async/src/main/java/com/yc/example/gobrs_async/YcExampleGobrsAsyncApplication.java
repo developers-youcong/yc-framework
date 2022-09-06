94b96a2b3ca1da6974d11aa565864479b24c4461
developers-youcong <15200740587@163.com>
2022-09-06 20:23:51 +0800

    save

---
+++ yc-example/yc-example-gobrs-async/src/main/java/com/yc/example/gobrs_async/YcExampleGobrsAsyncApplication.java
@@ -0,0 +1,16 @@
+package com.yc.example.gobrs_async;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: youcong
+ */
+@SpringBootApplication
+public class YcExampleGobrsAsyncApplication {
+    public static void main(String[] args)
+    {
+        SpringApplication.run(YcExampleGobrsAsyncApplication.class, args);
+    }
+}
